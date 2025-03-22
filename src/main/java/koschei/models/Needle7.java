b33c6820146879ad55dc50fce07cd6e2e1dc8046
ILdarez <ildareyy@gmail.com>
2025-03-22 22:44:32 +0500

    initial commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private Deth8 deth;
+
+    public Needle7() {
+        this.deth = new Deth8();
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
