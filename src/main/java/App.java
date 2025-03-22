8fb7b30ddc4f0a4e588cf4bc54793d7d63d75f9f
ILdarez <ildareyy@gmail.com>
2025-03-22 17:02:35 +0500

    initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat beanCatOne =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatOne.getName());
+        Cat beanCatTwo =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatTwo.getName());
+        System.out.println("Сравнение пары бинов HelloWorld" + " " + (bean == bean2));
+        System.out.println("Сравнение пары бинов Cat" + " " + (beanCatOne == beanCatTwo));
     }
 }
